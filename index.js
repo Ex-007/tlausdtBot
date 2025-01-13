a471f5541b24e1bb9b9cf5af2918f47c64f7aff9
Ex-007 <agbebibidemi02@gmail.com>
2025-01-13 14:10:40 +0100

    constructed the trial version, about and help

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ bot.onText(/\/start/, (msg) => {
     const options = {
         reply_markup: JSON.stringify({
             inline_keyboard: [
-                [{ text: 'Get Started', callback_data: 'getStarted' }, { text: 'Subscriptions', callback_data: 'subscriptions' }],
+                [{ text: 'About', callback_data: 'about' }, { text: 'Subscriptions', callback_data: 'subscriptions' }],
                 [{ text: 'Visit Website', url: 'https://www.tlausdtfiles.pro' }],
-                [{ text: 'Help', callback_data: 'help' }, { text: 'About', callback_data: 'about' }],
+                [{ text: 'Help', callback_data: 'help' }, { text: 'FAQs', callback_data: 'askedQuestions' }],
             ]
         })
     }
@@ -29,4 +29,52 @@ bot.onText(/\/start/, (msg) => {
 })
 
 // THE HELP FUNCTION
-bot.on
+bot.on('callback_query', (query) => {
+    let chatId = query.message.chat.id
+    let data = query.data
+
+    if(data === 'help') {
+
+
+        const helpMessage = `
+        HELP:
+        \nStep 1 \nCheck the licenses in the subscription page;
+        \nStep 2 \nSelect the license that suit you;
+        \nStep 3 \nPurchase the license selected;
+        \nStep 4 \nLogin and generate the product key \nCheck your mail for the key;
+        \nStep 5 \nSet transaction and broadcast the it. \nEnjoy your USDT
+        `
+        const trialVersion = {
+            reply_markup: JSON.stringify({
+                inline_keyboard: [
+                    [{ text: 'Check Trial Version', callback_data: 'trialVersion' }]
+                ]
+            })
+        }
+        bot.sendMessage(chatId, helpMessage, trialVersion)
+
+
+    }else if(data === 'about') {
+        const aboutMessage = `
+        ABOUT:
+        \nWelcome to TLAUSDT, your premier destination for USDT flashing.
+        \nWe are here to help you get started with USDT flashing.
+        \nWe offer a variety of license plans to help you get started.
+        `
+        bot.sendMessage(chatId, aboutMessage)
+
+
+    }else if(data === 'subscriptions') {
+        bot.sendMessage(chatId, 'This is the subscription section')
+    }else if(data === 'trialVersion') {
+
+        const trialVersionMessage = `
+        TRIAL VERSION:
+        \nThis is the trial version of the software.
+        \nThis Demo allows you to experience the reliability and functionality of our software before committing to the full paid version.
+        \nThis Demo comes with a one-time use license.
+        \nThis Demo is limited to sending 3 USDT per day.
+        `
+            bot.sendMessage(chatId, trialVersionMessage)
+    }
+})
